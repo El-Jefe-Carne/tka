61a90d42a6bb259c54a64837bd235ed57fd37353
El_Jefe_Carne <jeff.budnick@gmail.com>
2014-09-14 18:09:51 -0400

    Added custom js and css files

---
+++ js/global.js
@@ -0,0 +1,15 @@
+﻿// A $( document ).ready() block.
+$(document).ready(function () {
+    //set jumbotron height
+    $('.jumbotron').css('height', $(window).height() + 'px');
+    $('#about').css('height', $(window).height() * .8 + 'px');
+
+    $("#jumbo-table").addClass("set-table");
+    $("#jumbo-cell").addClass("set-cell");
+
+    //when the window resizes, resize the divs.
+    $(window).resize(function(){
+        $('.jumbotron').css('height', $(window).height() + 'px');
+        $('#about').css('height', $(window).height() * .8 + 'px');
+    });    
+});
